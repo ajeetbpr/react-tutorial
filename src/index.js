d58c119e7c9ec45e5a16433fca07ee749599d961
imajeet <ajeetbpr@gmail.com>
2018-05-20 09:55:32 +0800

    Increment App

--- src/index.js
+++ src/index.js
@@ -1,21 +1,58 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-function Cartoon(props){
-   return<h1>hello, {props.name} on {props.show}</h1>
-}
 
-function Show(){
-	return <div>
-	<Cartoon name='Ajeey' show='pokemon' />
-	<Cartoon name='Awrwerw' show='porwrwon' />
-	</div>
-}
+// class Clock extends React.Component{
+// 	constructor(props){
+// 		super(props)
+// 		this.state = { 
+//           date : new Date()
+// 		}
+// 	}
+
+// 	componentDidMount(){
+//         setInterval( ()=> this.start(), 1000);
+// 	}
+// 	componentWillUnmount(){
+
+// 	}
+// 	start(){
+// 		this.setState({
+// 			date: new Date()
+// 		});
+// 	}
+//    render(){
+//    	return <h1>Time is : {this.state.date.toLocaleTimeString()}</h1>
+//    }
+// }
+
+
+class Inc extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = { counter : 0}
+		this.increment = this.increment.bind(this);
+	}
+
+	render(){
+		return <button onClick= {this.increment}>Value is {this.state.counter}</button>
+	}
+
+	increment = (e) => {
+		e.preventDefault();
+		this.setState({
+           counter : this.state.counter + 1
+		});
+	}
+} 
 
 
 ReactDom.render(
-<Show/>,
+<Inc/>,
 document.getElementById('root')
 );
+
+
+
 
 
