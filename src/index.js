8ff4c02fecbce38f98592690bdda3ac2a5d87606
imajeet <ajeetbpr@gmail.com>
2018-05-21 09:54:55 +0800

    List-Item

--- src/index.js
+++ src/index.js
@@ -1,38 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-function Message(props){
-	if(props.value){
-      return <h1>This is first  Message</h1>
-	}
-
-	return <h1>I am from the second  Message</h1>
+function ToonList(props){
+	const list  = props.cartoon
+	const toons = list.map((list, index)=> <li key={index}>{list}</li>)
+    return <ul>{
+    	list.map((list, index)=> <li key={index}>{list}</li>)
+    }</ul>
 }
-
-class Btn extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {value: true}
-	}
-
-	handleClick = () =>{
-		this.setState({
-			value : !this.state.value
-		});
-	}
-
-	render(){
-		return(
-			<div>
-			<button onClick={this.handleClick}>Change this Message</button>
-		    <Message value = {this.state.value} />
-			</div>
-			) 
-	}
-}
+const cartoons = ['Pikachu', 'Alladin', 'Tom', 'Tom']
 
 ReactDom.render(
-<Btn/>,
+<ToonList cartoon={cartoons} />,
 document.getElementById('root')
 );
 
