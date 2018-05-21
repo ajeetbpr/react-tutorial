93b4c3b236310f029f78b901168f69c7585cc35a
imajeet <ajeetbpr@gmail.com>
2018-05-21 11:33:26 +0800

    Lifting up state for Temp Scale

--- src/index.js
+++ src/index.js
@@ -20,20 +20,37 @@ handleFehrenheit = (e)=> {
 	render(){
 		const temp = this.state.temp
 		const scale = this.state.scale
-		const celsius = scale === 'f' ? (temp - 32)*5/9 : temp;
-		const fehrenheit = scale === 'c' ? (temp* 9/5) + 32 : temp;
+		const celsius = scale === 'f' ? convert(temp, toCelsius) : temp;
+		const fehrenheit = scale === 'c' ? convert(temp, toFehrenheit) : temp;
 		return(
 			<div>
-           <fieldset>
-           <legend>Scale Celsius</legend>
-           <input value={celsius} onChange={this.handleCelsius}/>
-           </fieldset>
-
-           <fieldset>
-           <legend>Scale Fehrenheit</legend>
-           <input value={fehrenheit} onChange={this.handleFehrenheit}/>
-           </fieldset>
+           
+           <Inputs scalename="Celsius" value={celsius} func={this.handleCelsius}/>
+           <Inputs scalename="Fehrenheit" value={fehrenheit} func={this.handleFehrenheit}/>
            </div>
+		)
+	}
+}
+
+function convert(temp, convertFunction){
+  return convertFunction(temp)
+}
+
+function toCelsius(fehrenheit){
+  return(fehrenheit - 32)*5/9
+}
+
+function toFehrenheit(celsius){
+	return(celsius * 9/5) + 32
+}
+
+class Inputs extends React.Component{
+	render(){
+		return(
+			<fieldset>
+           <legend>Scale this.props.scalename</legend>
+           <input value={this.props.value} onChange={this.props.func}/>
+           </fieldset>
 		)
 	}
 }
